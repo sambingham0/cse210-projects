d759af20fdfc84e66ff09e24d1a062ecc38de295
sambingham0 <sambingham3143@gmail.com>
2024-01-19 21:56:54 -0700

    example problems

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        
+        int guess = -1;
+        int i = 0;
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            i = i+1;
+        }
+        Console.WriteLine($"You got it in {i} guesses.");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<float> num_list = new List<float>();
+        float user_number = -1;
+        float sum = 0;
+        float avg = 0;
+        float i = 0;
+        float largest = 0;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (user_number != 0)
+        {
+            user_number = float.Parse(Console.ReadLine());
+            num_list.Add(user_number);
+        }
+        foreach (float num in num_list)
+        {
+            sum += num;
+            i++;
+            if (num > largest)
+            {
+                largest = num;
+            }
+        }
+        i--;
+        Console.WriteLine($"The sum is: {sum}.");
+        avg = sum / i;
+        Console.WriteLine($"The average is: {avg}.");
+        Console.WriteLine($"The largest number is: {largest}.");
     }
 }
