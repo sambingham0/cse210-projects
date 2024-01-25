eeccacb4e1ca4ea5b9bf670c1ce29fbb72b76422
sambingham0 <sambingham3143@gmail.com>
2024-01-24 17:46:52 -0700

    W03 Prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,26 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Denny's";
+        job1._jobTitle = "Server";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._company = "Papa John's";
+        job2._jobTitle = "Cook";
+        job2._startYear = 2018;
+        job2._endYear = 2019;
+
+        Resume myResume = new Resume();
+        myResume._name = "Sam Bingham";
+        
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
